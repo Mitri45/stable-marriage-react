546f3a5628a954fd22963608aa2f421679f3d3fd
unknown <dmitrii.website@gmail.com>
2020-08-13 20:58:29 +1200

    Include build directory for Heroku

---
+++ frontend/build/precache-manifest.c805acd2949dc07916dc6936390f4324.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ec925731b5d0a4d1a3175f8482e72cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98b35a54895407dc7c43",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2a2b456852505bfb185f",
+    "url": "/static/js/2.e9dadafb.chunk.js"
+  },
+  {
+    "revision": "5c9363e977ff41d46d65dfd35cd7efcc",
+    "url": "/static/js/2.e9dadafb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98b35a54895407dc7c43",
+    "url": "/static/js/main.59c4c44a.chunk.js"
+  },
+  {
+    "revision": "e7035f46302c1ed74d95",
+    "url": "/static/js/runtime-main.4c01b37b.js"
+  },
+  {
+    "revision": "eaa32adc61b028ba09ef759fbeb12171",
+    "url": "/static/media/reorder.eaa32adc.svg"
+  },
+  {
+    "revision": "330f6514182716c5d90b3e8ef984e923",
+    "url": "/static/media/userAvatar.330f6514.png"
+  }
+]);
